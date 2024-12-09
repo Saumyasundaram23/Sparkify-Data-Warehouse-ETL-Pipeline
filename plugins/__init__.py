74a2484a08216fff0ce3ba555794afb59e38c6ea
Saumyasundaram23 <160294204+Saumyasundaram23@users.noreply.github.com>
2024-12-09 17:37:00 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4,7 +4,7 @@ from airflow.plugins_manager import AirflowPlugin
 
 import operators
 import helpers
-
+#importing modules
 # Defining the plugin class
 class SparkifyPlugin(AirflowPlugin):
     name = "sparkify_plugin"
